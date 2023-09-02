b7a0503a57ed3aef789bb7642c0b1f35dc599309
bechara0 <jjbechara@gmail.come>
2023-09-02 19:19:27 -0300

    recien inicidado

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>CRM react - Create React DOM</h1>
+    </>
+  );
+}
+
+export default App;
